ce2470eec82dad4689a12139cdc2d9489c48ebd0
Łukasz <lukstasiak26@wp.pl>
2018-02-25 18:53:59 +0100

    pracownia
    zadanie1

---
+++ Zadanie1/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <stdexcept>
+using namespace std;
+typedef int64_t int64;
+
+bool czyPierwsza (int64 liczba)
+{
+    int temp=liczba;
+    if(liczba<2)
+        return false; //gdy liczba jest mniejsza ni¿ 2 to nie jest pierwsz¹
+    for(int i=2; i<=sqrt(temp); i++)
+        if(liczba%i==0)
+            return false; //gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza
+    return true;
+
+}
+
+int64 StringToInt64(string wejscie)
+{
+    try
+    {
+        return stoll(wejscie);
+    }
+    catch (const invalid_argument )
+    {
+        cerr << "Bledny argument";
+        exit(0);
+    }
+}
+
+vector <int64> Czynniki(int64 liczba)
+{
+    vector <int64> odp;
+    int64 zakres = -9223372036854775808;
+    if(liczba==zakres) cout << "-1*";
+    if(liczba<0 && liczba!=zakres)
+    {
+        cout << "-1*";
+        liczba=liczba*-1;
+    }
+    while (liczba%2 == 0)
+    {
+        odp.push_back(2);
+        liczba = liczba/2;
+    }
+    for (int64 i = 3; i <= sqrt(liczba); i = i+2)
+    {
+        while (liczba%i == 0)
+        {
+            odp.push_back(i);
+            liczba = liczba/i;
+        }
+    }
+    if (liczba > 2) odp.push_back(liczba);
+    return odp;
+}
+
+int main(int argc, char * argv[])
+{
+    if(argc<2)
+    {
+        cout << "Wpisz liczbe z przedzialu [-9,223,372,036,854,775,808 ; 9,223,372,036,854,775,807] ";
+        return 0;
+    }
+    int64 liczba=StringToInt64(argv[1]);
+    if(liczba==-1 || liczba==1 || liczba ==0)
+    {
+        cout <<liczba << " = " <<liczba;
+        return 0;
+    }
+    cout <<liczba<<" = ";
+    vector <int64> odp=Czynniki(liczba);
+    for(unsigned int i=0; i<odp.size(); i++)
+    {
+        cout<<odp[i];
+        if(i!=odp.size()-1) cout << "*";
+    }
+    return 0;
+}
